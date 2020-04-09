ecb7074346e1a634b9075bef0b47fceaf4a28252
Arinjay Salgia <asalgia@flexera.com>
2020-04-09 08:46:25 -0500

    adding libraries and sample.

---
+++ SAP-HANA-AWS-CFT/sap_hana_cft_cat.rb
@@ -0,0 +1,6 @@
+name "SAP HANA CFT Deployment CAT"
+rs_ca_ver 20161221
+short_description  "SAP HANA CFT Deployment CAT"
+long_description "This CAT deploys SAP HANA on AWS using the AWS Cloud Formation template"
+
+import "plugins/rs_aws_cft"
