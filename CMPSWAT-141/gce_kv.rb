3b353234b036b27e7e54685e913689ee85b8415e
Richard Shade <rshade@flexera.com>
2020-08-03 15:47:20 -0500

    updating to support multiple file and docs

--- CMPSWAT-141/gce_kv.rb
+++ CMPSWAT-141/gce_kv.rb
@@ -5,12 +5,37 @@ package "google/kv_utils"
 import "sys_log"
 import "google/cloud_storage"
 
-define initialize($bucket,$object) do
+# This function initializes the global variable $$workflow_kv_store for later uses
+# Usage: 
+# call kv_utils.initialize("secret_bucket",["base.json"])
+# call kv_utils.initialize("secret_bucket",["base.json","production.json"])
+
+define initialize($bucket,$objects) do
   $$workflow_kv_store = {}
-  $credential_file = google.cloud_storage.get(bucket: $bucket, object: $object )
-  $$workflow_kv_store = from_json($credential_file)
+  foreach $object in $objects do
+    $credential_file = google.cloud_storage.get(bucket: $bucket, object: $object)
+    $hash_credential_file = from_json($credential_file)
+    foreach $key in $hash_credential_file do
+      $$workflow_kv_store[$key] = $hash_credential_file[$key]
+    end
+  end
 end
 
+# This function merges an additional file into the $$workflow_kv_store variable,
+# it will overwrite existing keys. Its preferrable to initialize all in the beginning.
+# Usage: 
+# call kv_utils.merge("secret_bucket","base.json")
+define merge($bucket, $object) do
+  $additional_file = google.cloud_storage.get(bucket: $bucket, object: $object)
+  $hash_additional_file = from_json($additional_file)
+  foreach $key in $hash_additional_file do
+    $$workflow_kv_store[$key] = $hash_additional_file[$key]
+  end
+end
+
+# This function is used to retrieve a value from the key value global variable $$workflow_kv_store
+# Usage: 
+# call kv_utils.get("username") retrieve $username
 define get($key) return $value do
   $value = $$workflow_kv_store[$key]
 end
