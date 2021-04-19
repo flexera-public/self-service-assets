a282d341fa27f06a85ec0b2bfd342ce9df9584e4
Richard Shade <rshade@flexera.com>
2021-04-19 10:27:41 -0500

    updating simple compute

--- self-service-plugin-based/aws/rs_aws_compute/simple_compute_example_linux.rb
+++ self-service-plugin-based/aws/rs_aws_compute/simple_compute_example_linux.rb
@@ -2,35 +2,61 @@ name 'AWS Simple Linux Instance'
 rs_ca_ver 20161221
 short_description "AWS EC2 Linux Instance"
 import "sys_log"
-import "plugin/rs_aws_compute"
+import "plugin/aws_compute"
 
 parameter "param_region" do
   like $aws_compute.param_region
+  default "us-east-2"
 end
 
 parameter "param_ami" do
   label "AMI Id"
   type "string"
+  default "ami-03c097d564dea7d12"
 end
 
 parameter "param_instance_type" do
   label "Instance Type"
   type "string"
+  default "t3.medium"
 end
 
 parameter "param_subnet_id" do
   label "Subnet Id"
   type "string"
+  default "subnet-3c9fb176"
 end
 
 parameter "param_key_name" do
   label "Key Name"
   type "string"
+  default "default"
 end
 
 parameter "param_az" do
   label "Availability Zone"
   type "string"
+  default "us-east-2c"
+end
+
+parameter "param_volume_size" do
+  label "Volume Size"
+  type "string"
+  default "100"
+end
+
+resource "instance", type: "aws_compute.instances" do
+  image_id $param_ami
+  instance_type $param_instance_type
+  subnet_id $param_subnet_id
+  key_name $param_key_name
+  min_count "1"
+  max_count "1"
+  placement_availability_zone $param_az
+  placement_tenancy "default"
+  tag_specification_1_resource_type "instance"
+  tag_specification_1_tag_1_key "Name"
+  tag_specification_1_tag_1_value @@deployment.name
 end
 
 operation "launch" do
